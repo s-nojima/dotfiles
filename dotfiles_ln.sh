92a7910df410350a278214b5ac4f61ab8d676557
s-nojima <s-nojima@svop.sakura.ad.jp>
2015-04-08 13:28:42 +0900

    neocomp php

--- dotfiles_ln.sh
+++ dotfiles_ln.sh
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/.conkyrc ~/.conkyrc
 ln -sf ~/dotfiles/.conkyrc_clock ~/.conkyrc_clock
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.lftprc ~/.lftprc
 
 exit 0
