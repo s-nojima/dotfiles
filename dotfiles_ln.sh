137342f0d95b3c174c9e8fd3f03bad7c10bd29ae
red-c <red-c@c-system>
2015-05-24 20:20:31 +0900

    house conky

--- dotfiles_ln.sh
+++ dotfiles_ln.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-ln -sf ~/dotfiles/.conkyrc ~/.conkyrc
-ln -sf ~/dotfiles/.conkyrc_clock ~/.conkyrc_clock
 ln -sf ~/dotfiles/.screenrc ~/.screenrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.lftprc ~/.lftprc
