90dd1cc05b994391a6fdb2704d52b0bdbcf10ea9
NOJIMA, Shuhei <shuhei.nojima@gmail.com>
2020-06-28 23:23:47 +0900

    fix:うごくようにした

--- dotfiles_ln.sh
+++ dotfiles_ln.sh
@@ -5,5 +5,6 @@
 #ln -sf ~/dotfiles/.vim/ ~/.vim
 #ln -sf ~/dotfiles/.lftprc ~/.lftprc
 ln -sf ~/dotfiles/conky/conkyrc_redc ~/.conky/.conkyrc_redc
+ln -sf ~/dotfiles/conky/conkyrc_redc_rss ~/.conky/.conkyrc_redc_rss
 
 exit 0
