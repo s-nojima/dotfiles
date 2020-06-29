4b7bfb52e2235dbef3ba54da519e79de2d782041
NOJIMA, Shuhei <shuhei.nojima@gmail.com>
2020-06-29 11:51:23 +0900

    fix:いろいろ

--- conky/conky_startup.sh
+++ conky/conky_startup.sh
@@ -2,8 +2,9 @@
 
 sleep 5
 
-conky -c ~/dotfiles/conky_redc &
-conky -c ~/dotfiles/conky_redc_net &
-conky -c ~/dotfiles/conky_redc_rss &
- 
+#conky -c ~/dotfiles/conky_redc &
+#conky -c ~/dotfiles/conky_redc_net &
+#conky -c ~/dotfiles/conky_redc_rss &
+conky -d -c ~/.conky/.conkyrc_redc
+conky -d -c ~/.conky/.conkyrc_redc_rss
 exit
