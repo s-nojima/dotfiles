6dbbe0b5501ba71582adb0a3cc2119f3a1d5e21d
s-nojima <shuhei.nojima@gmail.com>
2021-08-19 13:45:05 +0900

    aaaaaaaaaaaaaaaaaa

---
+++ deinscript.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.cache/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+# For example, we just use `~/.cache/dein` as installation directory
+sh ./installer.sh ~/.cache/dein
